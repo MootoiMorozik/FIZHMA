164f0337ba37492f035af24c0bcebcf694aa01a8
Morozik <ochkozavr93@gmail.com>
2025-08-04 12:23:49 +0400

    Update server.py

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify, send_from_directory, request
 from datetime import datetime, timezone, timedelta
+import os
 
 app = Flask(__name__, static_folder='static')
 
@@ -33,4 +34,5 @@ def index():
     return send_from_directory('static', 'index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
